da68c03d0c681f80c90cbf6845e8865358704c98
Redream <mr.golyadkina@mail.ru>
2025-08-21 15:49:34 +0300

    added Singleton

---
+++ Assets/_Project/Logic/Scripts/General/Singleton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Instance;
+
+    protected virtual void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this as T;
+    }
+
+    protected virtual void OnApplicationQuit()
+    {
+        Instance = null;
+        Destroy(gameObject);
+    }
+}
