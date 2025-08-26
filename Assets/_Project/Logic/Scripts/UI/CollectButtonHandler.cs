6132a18365d9c9297835d712410860c2a4eb0aaa
Redream <mr.golyadkina@mail.ru>
2025-08-26 16:24:54 +0300

    Added rewarded adv

--- Assets/_Project/Logic/Scripts/UI/CollectButtonHandler.cs
+++ Assets/_Project/Logic/Scripts/UI/CollectButtonHandler.cs
@@ -1,7 +1,21 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using YG;
 
 public class CollectButtonHandler : MonoBehaviour
 {
+    private string rewardID = "DoubleMoney";
+
+    private void OnEnable()
+    {
+        YG2.onRewardAdv += OnReward;
+    }
+
+    private void OnDisable()
+    {
+        YG2.onRewardAdv -= OnReward;
+    }
+
     public void CollectButtonOnClick()
     {
         WalletSystem.Instance.AddMoneyPerCatch();
@@ -10,7 +24,15 @@ public class CollectButtonHandler : MonoBehaviour
 
     public void CollectDoubleButtonOnClick()
     {
-        WalletSystem.Instance.AddMoneyPerCatch(2);
-        ScreenSelectorSystem.Instance.ShowMainScreen();
+        YG2.RewardedAdvShow(rewardID);
+    }
+
+    private void OnReward(string id)
+    {
+        if(id == rewardID)
+        {
+            WalletSystem.Instance.AddMoneyPerCatch(2);
+            ScreenSelectorSystem.Instance.ShowMainScreen();
+        }
     }
 }
